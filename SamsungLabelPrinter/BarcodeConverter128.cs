bbab987ef0a7b2bf539239d007aa625d3e8a50d3
lesterxu <lester88a@gmail.com>
2016-02-24 15:24:46 -0500

    first commit after relase

---
+++ SamsungLabelPrinter/BarcodeConverter128.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SamsungLabelPrinter
+{
+    class BarcodeConverter128
+    {
+        public static string StringToBarcode(string value)
+        {
+            bool flag1 = true;
+            bool flag2 = true;
+            string str = string.Empty;
+            if (value.Length > 0)
+            {
+                for (int startIndex = 0; startIndex < value.Length; ++startIndex)
+                {
+                    int num = (int)char.Parse(value.Substring(startIndex, 1));
+                    if (num < 32 || num > 126)
+                    {
+                        flag2 = false;
+                        break;
+                    }
+                }
+                if (flag2)
+                {
+                    int num1 = 0;
+                    while (num1 < value.Length)
+                    {
+                        if (flag1)
+                        {
+                            int MinCharPos = num1 == 0 || num1 + 4 == value.Length ? 4 : 6;
+                            if (BarcodeConverter128.IsNumber(value, num1, MinCharPos) < 0)
+                            {
+                                str = num1 != 0 ? str + 'Ç'.ToString() : 'Í'.ToString();
+                                flag1 = false;
+                            }
+                            else if (num1 == 0)
+                                str = 'Ì'.ToString();
+                        }
+                        if (!flag1)
+                        {
+                            int MinCharPos = 2;
+                            if (BarcodeConverter128.IsNumber(value, num1, MinCharPos) < 0)
+                            {
+                                int num2 = int.Parse(value.Substring(num1, 2));
+                                int num3 = num2 < 95 ? num2 + 32 : num2 + 100;
+                                str += ((char)num3).ToString();
+                                num1 += 2;
+                            }
+                            else
+                            {
+                                str += 'È'.ToString();
+                                flag1 = true;
+                            }
+                        }
+                        if (flag1)
+                        {
+                            str += value.Substring(num1, 1);
+                            ++num1;
+                        }
+                    }
+                    int num4 = 0;
+                    for (int startIndex = 0; startIndex < str.Length; ++startIndex)
+                    {
+                        int num2 = (int)char.Parse(str.Substring(startIndex, 1));
+                        int num3 = num2 < (int)sbyte.MaxValue ? num2 - 32 : num2 - 100;
+                        num4 = startIndex != 0 ? (num4 + startIndex * num3) % 103 : num3;
+                    }
+                    int num5 = num4 < 95 ? num4 + 32 : num4 + 100;
+                    str = str + ((char)num5).ToString() + 'Î'.ToString();
+                }
+            }
+            return str;
+        }
+
+        private static int IsNumber(string InputValue, int CharPos, int MinCharPos)
+        {
+            --MinCharPos;
+            if (CharPos + MinCharPos < InputValue.Length)
+            {
+                while (MinCharPos >= 0 && ((int)char.Parse(InputValue.Substring(CharPos + MinCharPos, 1)) >= 48 && (int)char.Parse(InputValue.Substring(CharPos + MinCharPos, 1)) <= 57))
+                    --MinCharPos;
+            }
+            return MinCharPos;
+        }
+    }
+}
---
+++ SamsungLabelPrinter/MainWindow.xaml.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SamsungLabelPrinter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+       
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            //display the label printer
+            foreach (string printer in System.Drawing.Printing.PrinterSettings.InstalledPrinters)
+            {
+                listBox1.Items.Add(printer);
+            }
+            
+        }
+
+        //move window around
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+
+            // Begin dragging the window
+            this.DragMove();
+        }
+        //exit application
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        //switch text box
+        private void textBoxPONo_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                textBoxPartNo.Focus();
+
+                e.Handled = true;
+            }
+        }
+
+        private void textBoxPartNo_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                textBoxPONo.Focus();
+
+                e.Handled = true;
+            }
+        }
+
+        //boolean for check the generate button click
+        private bool button1WasClicked = false;
+
+        //generate button
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            //ensure the part order number is not empty
+            if (textBoxPONo.Text.Length > 15)
+            {
+                MessageBox.Show("Sorry, IMIE only accept 15 digits!");
+                textBoxPON.Text = "IMEI: " + "Error!";
+            }
+            else if (textBoxPONo.Text.Length <= 0)
+            {
+                MessageBox.Show("Sorry, IMIE cannot be empty!");
+                textBoxPON.Text = "IMEI: " + "Error!";
+            }
+            else
+            {
+                textBoxPON.Text = "IMEI: " + textBoxPONo.Text;
+
+                //ensure the part number is not empty
+                if (textBoxPartNo.Text.Length <= 0)
+                {
+                    MessageBox.Show("Sorry, part code. cannot be empty!");
+                    textBoxBarcode.Text = "*" + "Error!" + "*";
+                    textBoxPN.Text = "Part Code: " + "Error!";
+                }
+                else
+                {
+                    textBoxAll.Text = textBoxPONo.Text + textBoxPartNo.Text;
+                    textBoxBarcode.Text = textBoxPONo.Text + textBoxPartNo.Text;
+                    textBoxPN.Text = "Part Code: " + textBoxPartNo.Text;
+                    //textBoxDate.Text = DateTime.Now.ToString("MMM dd,yyyy");
+                    button1WasClicked = true;
+                }
+
+            }
+            //get selected copies
+            //set number of copies to print
+            int selectedValue = (int)comboBox1.SelectedIndex + 1;
+
+            //show selected printer name
+            //string printerName = listBox1.GetItemText(listBox1.SelectedItem);
+            try
+            {
+                string printerName = listBox1.SelectedItem.ToString();
+                toolStripStatusLabel1.Content = "Status:\t" + "Print Copies: " + selectedValue + ". Printer: " + printerName;
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Please select a printer!");
+                toolStripStatusLabel1.Content = "Status:\t" + "Please select a printer!";
+            }
+
+
+        }
+
+        private void btnPrint_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxPONo.Focus();
+
+            if (textBoxPONo.Text.Length > 15)
+            {
+                MessageBox.Show("Sorry, IMIE only accept maximum 15 digits!");
+            }
+            else if (textBoxPONo.Text.Length <= 0)
+            {
+                MessageBox.Show("Sorry, IMIE cannot be empty!");
+            }
+            else if (textBoxPartNo.Text.Length <= 0)
+            {
+                MessageBox.Show("Sorry, part code. cannot be empty!");
+            }
+            else if (button1WasClicked == false)
+            {
+                MessageBox.Show("Sorry, you have to generate before print!");
+            }
+            else
+            {
+                //get selected printer name
+                string printerName = listBox1.SelectedItem.ToString();
+
+                //print doc object
+                System.Drawing.Printing.PrintDocument myPrintDocument1 = new System.Drawing.Printing.PrintDocument();
+
+                //PrintDialog myPrinDialog1 = new PrintDialog();
+
+                myPrintDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(myPrintDocument2_PrintPage);
+
+                //use the selected printer name
+                myPrintDocument1.PrinterSettings.PrinterName = printerName;
+
+                //myPrintDocument1.DefaultPageSettings.Landscape = true;
+
+                //myPrintDocument1.DefaultPageSettings.PaperSize = new System.Drawing.Printing.PaperSize("Label2", 29, 20);
+
+                //set number of copies to print
+                int selectedValue = (int)comboBox1.SelectedIndex + 1;
+                myPrintDocument1.PrinterSettings.Copies = (short)selectedValue;
+                try
+                {
+                    toolStripStatusLabel1.Content = "Status:\t" + "Printing...";
+                    myPrintDocument1.Print();
+                    toolStripStatusLabel1.Content = "Status:\t" + "Succeed.";
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("Please select a printer!");
+                    toolStripStatusLabel1.Content = "Status:\t" + "Please select a printer!";
+                }
+
+            }
+
+            textBoxPONo.Text = "";
+            textBoxPartNo.Text = "";
+        }
+
+        private void myPrintDocument2_PrintPage(System.Object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            
+            //set font
+            Font printFont = new Font("Code 128", 22);
+            Font printFont2 = new Font("Arial", 6);
+            Font printFont3 = new Font("Arial", 5);
+
+            //set the bar-code width
+            Matrix m = new Matrix();
+            m.Scale(1F, 0.4F); //0.7 times height than original height
+
+            //begin scale the bar-code width
+            e.Graphics.Transform = m;
+            string textBoxBarcode128 = BarcodeConverter128.StringToBarcode(textBoxBarcode.Text);
+            e.Graphics.DrawString(textBoxBarcode128, printFont, System.Drawing.Brushes.Black, 2, 2, new StringFormat());
+            //reset the font to regular width
+            e.Graphics.ResetTransform();
+
+            e.Graphics.DrawString(textBoxBarcode.Text, printFont2, System.Drawing.Brushes.Black, 45, 13, new StringFormat());
+
+            e.Graphics.DrawString(textBoxPON.Text, printFont3, System.Drawing.Brushes.Black, 5, 22, new StringFormat());
+            e.Graphics.DrawString(textBoxPN.Text, printFont3, System.Drawing.Brushes.Black, 113, 22, new StringFormat());
+            //e.Graphics.DrawString(DateTime.Now.ToString("MMM dd,yyyy"), printFont2, System.Drawing.Brushes.Black, 175, 20, new StringFormat());
+
+
+        }
+    }
+}
